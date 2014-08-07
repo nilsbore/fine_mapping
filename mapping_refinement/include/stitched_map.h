87420894ada310bf705834bed706fe797524f8e7
Nils Bore <nbore@kth.se>
2014-08-07 13:24:06 +0200

    Forgot to add the actual files

---
+++ mapping_refinement/include/stitched_map.h
@@ -0,0 +1,24 @@
+#ifndef STITCHED_MAP_H
+#define STITCHED_MAP_H
+
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
+#include "scan.h"
+
+class stitched_map
+{
+private:
+    std::vector<scan*>& scans;
+    void average_scans(scan& scan1, scan& scan2, cv::Mat& counter1, cv::Mat& counter2);
+public:
+    void visualize();
+    stitched_map(std::vector<scan*>& scans) : scans(scans)
+    {
+
+    }
+};
+
+#endif // STITCHED_MAP_H
